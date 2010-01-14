865df0c65d0288833d16a563ecae4db49870d700
Pablo Lorenzoni <pablo@propus.com.br>
2010-01-14 18:11:04 -0200

    Implement a "problem rate" calculator and improve the test.rb

--- test.rb
+++ test.rb
@@ -1,42 +1,59 @@
 require 'rswiss.rb'
 require 'pp'
 players = ARGV[0].to_i
-p = [];0.upto(players) { |n| p << Player.new(n) }
-t = Tournament.new; p.each { |pl| t.add_player(pl) }
-t.begin!
-puts "\nNew tournment:"
+tournament = 0
+problems = 0
 while true
-	begin
-		m = t.get_next_match
-	rescue RuntimeError
-		break
-	end
-	rnd = rand(100)
-	if rnd < 20
-		# 20% chance of draw
-		m.decide(0)
-	elsif rnd >= 20 and rnd < 70
-		# 50% chance of the highest score win
-		if m.p1.score > m.p2.score
-			m.decide(1)
-		else
-			m.decide(2)
+	p = [];0.upto(players) { |n| p << Player.new(n) }
+	t = Tournament.new; p.each { |pl| t.add_player(pl) }
+	t.begin!
+	puts "\nNew tournment: #{tournament}"
+	while true
+		not_ended = false
+		begin
+			m = t.get_next_match
+		rescue RuntimeError
+			# Some error like end of tournament
+			break
+		rescue StandardError
+			# Fatal error
+			not_ended = true
+			break
 		end
+		rnd = rand(100)
+		if rnd < 20
+			# 20% chance of draw
+			m.decide(0)
+		elsif rnd >= 20 and rnd < 70
+			# 50% chance of the highest score win
+			if m.p1.score > m.p2.score
+				m.decide(1)
+			else
+				m.decide(2)
+			end
+		else
+			# 30% chance of the lowest score win
+			if m.p1.score > m.p2.score
+				m.decide(2)
+			else
+				m.decide(1)
+			end
+		end
+		t.put_match(m)
+	end
+	
+	if ! not_ended
+		pp t.final_chart
+		winner = t.winner
+		puts "The winner (by #{winner[1]}) is:"
+		pp winner[0]
 	else
-		# 30% chance of the lowest score win
-		if m.p1.score > m.p2.score
-			m.decide(2)
-		else
-			m.decide(1)
-		end
+		puts "Something bad happened"
+		problems += 1
 	end
-	t.put_match(m)
+	tournament += 1
+	puts ">>>> Problem rate: #{problems}/#{tournament}"
+	sleep 1
 end
-
-pp t.final_chart
-
-winner = t.winner
-puts "The winner (by #{winner[1]}) is:"
-pp winner[0]
 
 # vim: set ts=2:
